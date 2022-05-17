4c2dc8ae2b14181b262267c4f5b383fa6d6e73bf
GeviLearning <gevireddy007@gmail.com>
2022-05-17 17:51:30 +0530

    Updated ext mailer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -26,6 +26,7 @@ pipeline {
             echo 'This will run only if successful'
             emailext (
                 subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                to: 'gevireddy@gmail.com',
                 body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
                         <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
                 recipientProviders: [[$class: 'DevelopersRecipientProvider']]
