8fab65e4cf6518b9a5ddbacb63d361d2462f0926
GeviLearning <gevireddy007@gmail.com>
2022-05-17 17:49:00 +0530

    Updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,7 +24,12 @@ pipeline {
         }
         success {
             echo 'This will run only if successful'
-            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+            emailext (
+                subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+                        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+                recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+            )
         }
         failure {
             echo 'This will run only if failed'
